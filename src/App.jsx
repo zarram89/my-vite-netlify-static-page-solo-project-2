d73a80cb35e100434c12e1b9ccb93f4a35d281a5
Ramil <zarram89@gmail.com>
2022-12-10 20:18:42 +0500

    Adds components and styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div className="card">
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+function Main() {
+  return (
+    <div className="main">
+      <img className="main-img" src="../../img/user.jpg" width="317" height="317" alt="Ramil's photo" />
+      <div className="container">
+        <h1 className="main-name">Ramil Zaripov</h1>
+        <p className="main-career">Frontend Developer</p>
+        <a className="main-site-link" href="https://marvelous-arithmetic-fcd965.netlify.app" target="__blank">ramilzaripov.site</a>
+        <a className="main-email" href="mailto:zarram89@gmail.com">Email</a>
+        <h2 className="main-subtitle">About</h2>
+        <p className="main-text">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        <h2 className="main-subtitle">Interests</h2>
+        <p className="main-text">HTML, CSS, JS, TS, React, Node.js</p>
+      </div>
+    </div>
+  )
+}
+
+export default Main
